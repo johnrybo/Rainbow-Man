853cddd296426ee577f0dd812ba4a4542612e22b
John Rybo <68517441+johnrybo@users.noreply.github.com>
2020-12-16 16:06:22 +0100

    gubben kan röra sig i sidled och stannar på banan

--- src/gameController.ts
+++ src/gameController.ts
@@ -1,16 +1,20 @@
 class GameController {
     
     private road: Road;
+    private character: Character;
 
     constructor() {
         this.road = new Road();
+        this.character = new Character();
     }
 
     update() {
         this.road.update();
+        this.character.update();
     }
     
     draw() {
         this.road.draw();
+        this.character.draw();
     }
 }
