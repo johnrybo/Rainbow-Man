5c6f8fd536e2da85f36e3a24c27758a807f23dc9
Jennifer Tendell <jennifer.tendell@gmail.com>
2021-01-04 09:17:15 +0100

    new wall.

--- src/gameController.ts
+++ src/gameController.ts
@@ -6,7 +6,7 @@ class GameController {
 
     constructor() {
         this.road = new Road();
-        this.wall = new Wall();
+        this.wall = new Wall(3);
         this.character = new Character();
     }
 
