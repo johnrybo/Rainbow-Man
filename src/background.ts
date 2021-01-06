9bcec338d30a12bb2974243e4f0eb341c30f21a6
Nisa Tokmak <katnissa@gmail.com>
2021-01-06 10:23:53 +0100

    background branch was created

---
+++ src/background.ts
@@ -0,0 +1,33 @@
+class Background {
+    public background : Background[];
+    public totalColors : number; 
+    private backgroundWidth: number;
+    private backgroundHeight: number;
+    
+
+    constructor (totalColors : number) {
+        this.totalColors = totalColors;
+        this.background = changeBackground ();
+        this.backgroundWidth = width;        
+        this.totalColors = 10;
+        this.backgroundHeight = 100;
+
+    }
+
+    public changeBackground () : Background [];
+    const backgroundColors = ['red', 'blue', 'green', 'yellow'];
+        loadImage(backgroundColors, true);
+
+
+    
+    public update() {
+        // this.background.update();
+        
+    }
+
+    public draw (background) {
+
+        // for loop here to connect it with the highScore
+
+    }
+}
