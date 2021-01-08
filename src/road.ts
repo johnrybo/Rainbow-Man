3b11c16883fe54ae3478b056ab69e44601819c52
Jennifer Tendell <jennifer.tendell@gmail.com>
2021-01-08 11:36:48 +0100

    Add background to road, test background

--- src/road.ts
+++ src/road.ts
@@ -2,8 +2,11 @@ class Road {
     update() { }
 
     draw() {
-        background('blue');
+        image(backgroundLevel6, 0, 0, windowWidth, windowHeight);
         fill('lightgrey');
+        noStroke();
+        
         quad(width * .3, 100, width * .7, 100, width - 100, height, 100, height);
+        
     }
 }
