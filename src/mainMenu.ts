f2b01bed7e062636540aaefa9bb6821f6f890f47
Jennifer Tendell <jennifer.tendell@gmail.com>
2021-01-13 09:54:19 +0100

    Fix menus

--- src/mainMenu.ts
+++ src/mainMenu.ts
@@ -3,12 +3,14 @@ class MainMenu {
     private mainMenuImg: p5.Image;
     private rainbow: p5.Image;
     private controlsImg: p5.Image
+    private startButton!: p5.Element;
     
     constructor(game: IGameState) {
         this.game = game;
         this.mainMenuImg = backgroundLevel1;
         this.rainbow = rainbowImg;
         this.controlsImg = controlsImg;
+        this.addStartBtn();
     }
 
 
@@ -16,37 +18,36 @@ class MainMenu {
         push()
         image(this.mainMenuImg, 0, 0, width, height);
         imageMode(CENTER)
-        image(this.rainbow, width/2, 250, 600, height/2)
-        this.addStartBtn();
+        image(this.rainbow, width/2, 170, 600, 400)
+        
         this.showHighScore();
         this.showInstructions();
         this.showControls();
         pop()
     }
 
-    draw() { 
-    }
+    draw() { }
 
     private addStartBtn() {
-        let button = createButton('Start Game');
-
-        button.mousePressed(() => {
-            this.startGame(button);
+        this.startButton = createButton('Start Game');
+        
+        this.startButton.mousePressed(() => {
+            this.startGame();
         })
         
-        button.position(width/2 - 150, height/2 + 220);
-        button.style('font-size', '40px')
-        button.style('width', '300px')
-        button.style('height', '60px') 
-        button.style('border', 'none')
-        button.style('border-radius', '4px')
-        button.style('background-color', '#3BBA36')          
+        this.startButton.style('position', 'absolute')
+        this.startButton.style('justify-content', 'center')
+        this.startButton.style('font-size', '25px')
+        this.startButton.style('width', '200px')
+        this.startButton.style('height', '40px') 
+        this.startButton.style('border', 'none')
+        this.startButton.style('border-radius', '4px')
+        this.startButton.style('background-color', 'green')
+        this.startButton.style('color', 'white')          
     }
 
-    private startGame(button: p5.Element) {
-        game.gameState = "play";
-        button.elt.remove()
-        loop();
+    private startGame() {
+        this.game.changeGameState('play');
     }
 
     private showHighScore() {
@@ -59,15 +60,16 @@ class MainMenu {
 
     private showInstructions() {
         textAlign(CENTER)
-        textSize(width / 60);
-        text("RainBowMan can only go through the walls with the same color as himself.", width/2, height/2);
+        
+        textSize(width / 50);
+        text("RainBowMan can only go through the walls with the same color as himself.", width/2, height/2 + 60);
     }
 
     private showControls() {
         textAlign(CENTER)
-        textSize(width / 60);
-        text("Use left and right arrow to move.", width/2, height/2 + 50)
-        image(this.controlsImg, width/2, height/2 + 150, 300, 100)
+        textSize(width / 40);
+        text("Use left and right arrow to move.", width/2, height/2 + 100)
+        image(this.controlsImg, width/2, height/2 + 200, 300, 100)
     }   
 }    
         
